175a7c05b1e8b2777d6e7bb412c8419c1082e634
Richard Hitier <richard.hitier@co-libri.org>
2019-02-08 18:26:45 +0100

    Add rank to show_user subcommand

--- app/cli.py
+++ app/cli.py
@@ -69,20 +69,22 @@ def register(app):
     @og_adm.command()
     def show_users():
         """List all users in base"""
-        print ("{0:4} {1:14} {3:20} {2:30} {4:4}".format(
+        print ("{0:4} {1:14} {3:20} {2:30} {4:4} {5:4}".format(
                 'id', 
                 'username', 
                 'role', 
                 'email', 
-                'team_id', 
+                'team', 
+                'rank', 
                 ))
         for u in User.query.order_by(User.id).all():
-            print ("{0:4} {1:14} {3:20} {2:30} {4:4}".format(
+            print ("{0:4} {1:14} {3:20} {2:30} {4:4} {5:4}".format(
                 str(u.id or '---'),
                 str(u.username or '---'),
                 str(u.email or '---'),
                 str('---' if u.role is None  else  RolesType(u.role)),
-                str('---' if u.team_id is None  else  u.team_id)))
+                str('---' if u.team_id is None  else  u.team_id),
+                str('---' if u.player_rank is None  else  u.player_rank)))
 
     @app.cli.group()
     def translate():
