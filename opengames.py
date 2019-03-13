1421ee9012ad28f55ddf78fd59b21bb7743bf6d6
Richard Hitier <richard.hitier@co-libri.org>
2019-03-13 20:57:13 +0100

    Revert "Rename created app name"
    This reverts commit 3f2ccb4326ab8365a4aba130a058e67e84b6de5b.

--- opengames.py
+++ opengames.py
@@ -1,9 +1,9 @@
 from app import create_app, db, cli
 from app.models import User, Post
 
-opengamesapp = create_app()
-cli.register(opengamesapp)
+app = create_app()
+cli.register(app)
 
-@opengamesapp.shell_context_processor
+@app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
